6f072e2e0309624da6c13c4317592a81cfb24cfd
Emerson Harkin <emerson.f.harkin@gmail.com>
2020-05-19 12:12:46 -0400

    More error handling in parser and switch to BTreeMap neuron

--- src/parser.rs
+++ src/parser.rs
@@ -1,9 +1,13 @@
 use std::io::{BufReader, BufRead};
 use std::fs::File;
+use std::collections::{BTreeMap, btree_map::Entry};
 
-pub fn parse_file(file_name: String) -> Vec<SWCCompartment> {
+pub fn parse_file(file_name: String) -> SWCNeuron {
     let reader = get_file_reader(file_name);
-    parse_lines(reader)
+    match parse_lines(reader) {
+        Ok(neuron) => neuron,
+        Err(msg) => panic!(msg)
+    }
 }
 
 fn get_file_reader(file_name: String) -> BufReader<File> {
@@ -12,32 +16,58 @@ fn get_file_reader(file_name: String) -> BufReader<File> {
     return reader;
 }
 
-fn parse_lines(reader: BufReader<File>) -> Vec<SWCCompartment> {
-    let mut compartments = Vec::<SWCCompartment>::with_capacity(64);
+fn parse_lines(reader: BufReader<File>) -> Result<SWCNeuron, String> {
+    let mut neuron = SWCNeuron::new();
 
     for line in reader.lines() {
-        match line {
-            Ok(l) => {
-                if l.chars().next().unwrap() == '#' {
-                    // Line is a comment
-                    continue;
-                } else {
-                    let compartment = parse_line(l);
-                    compartments.push(compartment);
-                }
-            },
-            _ => panic!("Error reading line.")
+        match parse_line(line.expect("Could not read line."))? {
+            SWCLine::SWCCompartment(compartment) => neuron.try_insert(compartment)?,
+            SWCLine::Comment(_) => {}
         }
     }
 
-    return compartments;
+    return Ok(neuron);
 }
 
-fn parse_line(line: String) -> SWCCompartment {
-    let specs: Vec<&str> = line.split_whitespace().collect();
-    assert!(specs.len() == 7);
+fn parse_line(line: String) -> Result<SWCLine, String> {
+    let mut parse_result: SWCLine;
 
-    let id = specs[0].parse::<usize>().expect("Could not parse id");
+    if line.chars().next().unwrap() == '#' {
+        // Parse line as a comment, causing parse_result to be
+        // SWCLine::Comment
+        parse_result = SWCLine::Comment(line);
+    } else {
+        // Parse line as a compartment, causing parse_result to be
+        // SWCLine::SWCCompartment
+        parse_result = SWCLine::SWCCompartment(parse_line_as_compartment(line)?);
+    }
+
+    return Ok(parse_result);
+}
+
+enum SWCLine {
+    SWCCompartment(SWCCompartment),
+    Comment(String)
+}
+
+fn parse_line_as_compartment(line: String) -> Result<SWCCompartment, String> {
+    let specs: Vec<&str> = line.split_whitespace().collect();
+
+    // Check number of space-delimited items.
+    if specs.len() != 7 {
+        return Err(
+            format!("Expected 7 space-delimited items in compartment line,
+                got {} items instead.", specs.len())
+        )
+    }
+
+    let id: usize;
+    match specs[0].parse::<usize>() {
+        Ok(parsed_id) => id = parsed_id,
+        Err(_) => {
+            return Err(format!("Could not parse {} as a compartment id.", specs[0]))
+        }
+    }
     let compartment_kind = SWCCompartmentKind::from(specs[1].parse::<usize>().expect("Could not parse compartmentkind"));
     let position = Point {
         x: specs[2].parse::<f64>().expect("Could not parse x position"),
@@ -48,15 +78,48 @@ fn parse_line(line: String) -> SWCCompartment {
 
     let parent_id: Option<usize>;
     if specs[6].chars().next().unwrap() == '-' {
-        // Negative parent id means there is no parent.
+        // Negative parent id means there is no parent; this is the root of the
+        // neuron graph.
         parent_id = None;
     } else {
-        parent_id = Some(specs[6].parse::<usize>().expect(&format!("Could not parse parent id {}", specs[6])));
+        let parsed_parent_id = specs[6].parse::<usize>().expect(&format!("Could not parse parent id {}", specs[6]));
+        if parsed_parent_id >= id {
+            return Err(
+                format!("Expected parent_id for compartment {} to be less than {},
+                    got {} instead.", id, id, parsed_parent_id)
+            )
+        }
+        parent_id = Some(parsed_parent_id);
     }
 
-    SWCCompartment::new(id, compartment_kind, position, radius, parent_id)
+    return Ok(SWCCompartment::new(id, compartment_kind, position, radius, parent_id));
 }
 
+pub struct SWCNeuron {
+    compartments: BTreeMap<usize, SWCCompartment>
+}
+
+impl SWCNeuron {
+    fn new() -> SWCNeuron {
+        SWCNeuron {
+            compartments: BTreeMap::<usize, SWCCompartment>::new()
+        }
+    }
+
+    fn try_insert(&mut self, compartment: SWCCompartment) -> Result<(), String> {
+        match self.compartments.entry(compartment.id) {
+            Entry::Occupied(_) => Err(
+                format!("More than one compartment with id {} exists", compartment.id)
+            ),
+            Entry::Vacant(entry) => {
+                entry.insert(compartment);
+                Ok(())
+            }
+        }
+    }
+}
+
+#[derive(Copy, Clone)]
 pub struct SWCCompartment {
     pub id: usize,
     pub kind: SWCCompartmentKind,
@@ -77,12 +140,14 @@ impl SWCCompartment {
     }
 }
 
+#[derive(Copy, Clone)]
 pub struct Point {
     pub x: f64,
     pub y: f64,
     pub z: f64
 }
 
+#[derive(Copy, Clone)]
 pub enum SWCCompartmentKind {
     Undefined,
     Soma,
